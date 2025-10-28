c59961540645dafe98bccdd0c8eb1d908ac3acef
gustavozaan <gustavo.sbgd@gmail.com>
2025-10-28 01:23:38 +0000

    navios em diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -15,36 +15,80 @@ int main(){
     }
 
     //--- Navios ---
-    int navioHorizontal[3] = {3, 3, 3};
-    int navioVertical[3] = {3, 3, 3};
+    int navio1[3] = {3, 3, 3};
+    int navio2[3] = {3, 3, 3};
+    int navio3[3] = {3, 3, 3};
+    int navio4[3] = {3, 3, 3};
 
     
 
     //--- Cordenadas dos navios ---
-    int linhaH = 8;
-    int colunaH = 2;
-    int linhaV = 3;
-    int colunaV = 1;
+    int linhaH = 6; // HORIZONTAL
+    int colunaH = 8; // HORIZONTAL
 
-     //--- Posiciona o navio horizontal ---
+    int linhaV = 5; // VERTICAL
+    int colunaV = 1; // VERTICAL
+
+    int linhaD1 = 1; // DIAGONAL (\)
+    int colunaD1 = 1; // DIAGONAL (\)
+
+    int linhaD2 = 2; // VERTICAL (/)
+    int colunaD2 = 7; // VERTICAL (/)
+
+    //--- Verificação de sobreposição ---
+    #define livre(l, c) (tabuleiro [l][c] == 0)
+
+     //--- Posiciona o navio1 horizontal ---
     if (colunaH + tamanhoNavio <= tamanho) {
         for (int i = 0; i < tamanhoNavio; i++) {
-            tabuleiro[linhaH][colunaH + i] = navioHorizontal[i];
+            if (livre(linhaH, colunaH + i))
+            {
+               tabuleiro[linhaH][colunaH + i] = navio1[i];
+            } else {
+                printf("Erro: sobreposição detectada!\n");
+                return 1;
+            }
+            
         }
     }
 
-    //--- Posiciona o navio vertical ---
+    //--- Posiciona o navio2 vertical ---
     if (linhaV + tamanhoNavio <= tamanho) {
-        for (int i = 0; i < tamanhoNavio; i++) {
-            
-            // Evita sobreposição 
-            if (tabuleiro[linhaV + i][colunaV] == 0) {
-                tabuleiro[linhaV + i][colunaV] = navioVertical[i];
+        for (int i = 0; i < tamanhoNavio; i++) { 
+            if (livre(linhaV, colunaV + i))
+            {
+               tabuleiro[linhaV][colunaV + i] = navio2[i];
             } else {
                 printf("Erro: sobreposição detectada!\n");
                 return 1;
             }
         }
+    }
+
+    //--- Posiciona o navio3 diagonal (\) ---
+    if (linhaD1 + tamanhoNavio <= tamanho && colunaD1 + tamanhoNavio <= tamanho){
+        for (int i = 0; i < tamanhoNavio; i++)
+        {
+            if (livre(linhaD1 + i, colunaD1 + i))
+            {
+                tabuleiro [linhaD1 + i][colunaD1 + i] =  navio3[i];
+            }
+            
+        }
+        
+    }
+
+    //--- Posiciona o navio4 diagonal (/) ---
+    if (linhaD2 + tamanhoNavio <= tamanho && colunaD2 + tamanhoNavio <= tamanho){
+        for (int i = 0; i < tamanhoNavio; i++)
+        {
+            if (livre(linhaD2 + i, colunaD2 + i))
+            {
+                tabuleiro [linhaD2 + i][colunaD2 + i] =  navio4[i];
+            }
+            
+        }
+        
     }
 
 
